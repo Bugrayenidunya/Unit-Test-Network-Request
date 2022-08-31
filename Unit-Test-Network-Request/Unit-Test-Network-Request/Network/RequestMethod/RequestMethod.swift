0ced35e7cb387d5ad6222010e7152d5e4496b40d
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2022-08-31 14:47:44 +0300

    Add RequestMethod enum

---
+++ Unit-Test-Network-Request/Unit-Test-Network-Request/Network/RequestMethod/RequestMethod.swift
@@ -0,0 +1,14 @@
+//
+//  RequestMethod.swift
+//  Unit-Test-Network-RequestTests
+//
+//  Created by Enes Buğra Yenidünya on 31.08.2022.
+//
+
+import Foundation
+
+/// Use this enum to determine your request method when making a network call
+enum RequestMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
