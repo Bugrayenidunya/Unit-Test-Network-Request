9e32cb2888e28573976cada82c298989bede14e2
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2022-08-31 21:19:45 +0300

    Add MockURLProtocol

---
+++ Unit-Test-Network-Request/Unit-Test-Network-RequestTests/Mocks/MockURLProtocol.swift
@@ -0,0 +1,43 @@
+//
+//  MockURLProtocol.swift
+//  Unit-Test-Network-RequestTests
+//
+//  Created by Enes Buğra Yenidünya on 31.08.2022.
+//
+
+import Foundation
+
+class MockURLProtocol: URLProtocol {
+    
+    /// A dictionary of a mock data
+    static var requestHandler: ((URLRequest) throws -> (HTTPURLResponse, Data))?
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override class func canInit(with task: URLSessionTask) -> Bool {
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    override func startLoading() {
+        guard let handler = MockURLProtocol.requestHandler else { return }
+        
+        do {
+            let (response, data) = try handler(request)
+            
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            client?.urlProtocol(self, didLoad: data)
+            client?.urlProtocolDidFinishLoading(self)
+            
+        } catch {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+    }
+    
+    override func stopLoading() { }
+}
